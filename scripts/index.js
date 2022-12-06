5c1018867736e739665b692b98111bffd9405b27
Andronic123 <fotopoulos.andrew@gmail.com>
2022-12-06 10:19:33 +0200

    Formatted documents

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,46 @@
-import { update as updateSnake, 
-         draw as drawSnake, 
-         SNAKE_SPEED, 
-         getSnakeHead, 
-         snakeIntersection 
-        } from './snake.js'
-import { update as updateFood, 
-         draw as drawFood,
-         score
-        } from './food.js'
-import { outsideGrid } from './grid.js'
-let lastRenderTime = 0
-let gameOver = false
-const gameBoard = document.getElementById('game-board')
-const scores = document.getElementById('scores')
-
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+import { update as updateFood, draw as drawFood, score } from "./food.js";
+import { outsideGrid } from "./grid.js";
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
+const scores = document.getElementById("scores");
 
 function main(currentTime) {
-    if (gameOver) {
-        alert(`Game over. Your score is: ${score}`)
-            return window.location.reload()
-    }
-    
+  if (gameOver) {
+    alert(`Game over. Your score is: ${score}`);
+    return window.location.reload();
+  }
 
-    window.requestAnimationFrame(main)
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
-    lastRenderTime = currentTime
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  lastRenderTime = currentTime;
 
-    update()
-    draw()
-    scores.innerHTML = `${score}`
-} 
-window.requestAnimationFrame(main)
+  update();
+  draw();
+  scores.innerHTML = `${score}`;
+}
+window.requestAnimationFrame(main);
 
 function update() {
-    updateSnake()
-    updateFood()
-    checkDeath()
+  updateSnake();
+  updateFood();
+  checkDeath();
 }
 
 function draw() {
-    gameBoard.innerHTML = ''
-    drawSnake(gameBoard)
-    drawFood(gameBoard)
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
 }
 
 function checkDeath() {
-    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
